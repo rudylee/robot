0e252b33b2cbd374fe409bbbf923747fad523d86
Rudy Lee <rudylee1988@gmail.com>
2014-10-05 12:56:44 +1100

    Add test_data file and feature to read file

--- bin/robot.rb
+++ bin/robot.rb
@@ -3,7 +3,14 @@
 require "../lib/robot.rb"
 require "../lib/table.rb"
 
-table = Table.new(5,5)
-robot = Robot.new(1,1,"S")
+filename = ARGV.first
 
-robot.report
+file = File.open(filename, "r")
+file.each_line do |line|
+  puts line
+end
+file.close
+
+table = Table.new(5,5)
+robot = Robot.new(1,1,"SOUTH")
+
--- lib/robot.rb
+++ lib/robot.rb
@@ -5,6 +5,10 @@ class Robot
     @f = f
   end
 
+  def move
+
+  end
+
   def report
     puts @x.to_s << ' ' << @y.to_s << ' ' << @f.to_s
   end
